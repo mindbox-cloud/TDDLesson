bdf319da441388f9bc44a9d701db7a19fa898353
Ilya Krupcov <secretman44444@gmail.com>
2024-05-23 11:41:52 +0300

    Team5 task

--- TDDLesson.BankAccount.Tests/BankAccountTests.cs
+++ TDDLesson.BankAccount.Tests/BankAccountTests.cs
@@ -5,4 +5,66 @@ using FluentAssertions;
 [TestClass]
 public sealed class BankAccountTests
 {
+    [TestMethod]
+    public void BalanceZero_WhenBankAccountCreated()
+    {
+        var account = new BankAccount();
+
+        account.Balance.Should().Be(0);
+    }
+    
+    [TestMethod]
+    public void AddMoney_WhenMoneyIsGraterThanZero()
+    {
+        var account = new BankAccount();
+        var money = 1;
+        
+        account.Add(money);
+
+        account.Balance.Should().Be(money);
+    }
+    
+    [TestMethod]
+    public void FailAdd_WhenMoneyIsLessThanZero()
+    {
+        var account = new BankAccount();
+        var money = 0;
+        
+        var act = () => account.Add(money);
+
+        act.Should().Throw<InvalidOperationException>().WithMessage("Money should be more zero");
+    }
+    
+    [TestMethod]
+    public void TakeMoney_WhenBalanceSufficient()
+    {
+        var account = new BankAccount(1);
+        var money = 1;
+        
+        account.Take(money);
+
+        account.Balance.Should().Be(0);
+    }
+    
+    [TestMethod]
+    public void FailTakeMoney_WhenMoneyIsNegative()
+    {
+        var account = new BankAccount(1);
+        var money = -1;
+        
+        var act = () => account.Take(money);
+
+        act.Should().Throw<InvalidOperationException>().WithMessage("Money should be more zero");
+    }
+    
+    [TestMethod]
+    public void FailTakeMoney_WhenMoneyInsufficient()
+    {
+        var account = new BankAccount(1);
+        var money = 2;
+        
+        var act = () => account.Take(money);
+
+        act.Should().Throw<InvalidOperationException>();
+    }
 }
--- TDDLesson.BankAccount/BankAccount.cs
+++ TDDLesson.BankAccount/BankAccount.cs
@@ -4,10 +4,29 @@ public sealed class BankAccount
 {
     public int Balance { get; private set; }
 
+    public BankAccount(int balance = 0)
+    {
+        Balance = balance;
+    }
+
     public void Add(int money)
     {
-        if (money < 0) throw new InvalidOperationException();
+        ValidateMoney(money);
         
-        Balance = money;
+        Balance += money;
+    }
+
+    public void Take(int money)
+    {
+        ValidateMoney(money);
+        
+        if (Balance < money) throw new InvalidOperationException();
+        
+        Balance -= money;
+    }
+
+    private void ValidateMoney(int money)
+    {
+        if (money <= 0) throw new InvalidOperationException("Money should be more than zero");
     }
 }
