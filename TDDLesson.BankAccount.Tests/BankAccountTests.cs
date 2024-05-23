4e528ef553b69132078a7ea28c9d5c24a9f7c337
aalyoona <gaynutdinova@mindbox.cloud>
2024-05-23 11:42:23 +0300

    add impl

--- TDDLesson.BankAccount.Tests/BankAccountTests.cs
+++ TDDLesson.BankAccount.Tests/BankAccountTests.cs
@@ -5,4 +5,63 @@ using FluentAssertions;
 [TestClass]
 public sealed class BankAccountTests
 {
+    [TestMethod]
+    [DataRow(0, 5, 5)]
+    [DataRow(5, 5, 10)]
+    public void AddPositiveMoneyToBalance_MoneyAdded(int initialBalance, int money, int expected)
+    {
+        var account = new BankAccount
+        {
+            Balance = initialBalance
+        };
+        
+        account.Add(money);
+        
+        Assert.AreEqual(expected, account.Balance);
+    }
+    
+    [TestMethod]
+    [DataRow(-1)]
+    public void AddNegativeMoneyToBalance_ShouldThrowArgumentException(int money)
+    {
+        var account = new BankAccount();
+        
+        Assert.ThrowsException<ArgumentException>(() => account.Add(money));
+    }
+    
+    [TestMethod]
+    [DataRow(5, 5, 0)]
+    public void WithdrawalsPositiveMoneyFromBalance_MoneyDecreased(int initialBalance, int money, int expected)
+    {
+        var account = new BankAccount
+        {
+            Balance = initialBalance
+        };
+        
+        account.Withdrawals(money);
+        
+        Assert.AreEqual(expected, account.Balance);
+    }
+    
+    [TestMethod]
+    [DataRow(5, -5)]
+    public void WithdrawalsNegativeMoneyFromBalance_ShouldThrowArgumentException(int initialBalance, int money)
+    {
+        var account = new BankAccount
+        {
+            Balance = initialBalance
+        };
+        
+        Assert.ThrowsException<ArgumentException>(() => account.Withdrawals(money));
+        Assert.AreEqual(initialBalance, account.Balance);
+    }
+    
+    [TestMethod]
+    [DataRow(5)]
+    public void WithdrawalsMoneyFromBalance_BalanceEqualsZero_ShouldThrowInvalidOperationException(int money)
+    {
+        var account = new BankAccount();
+        
+        Assert.ThrowsException<InvalidOperationException>(() => account.Withdrawals(money));
+    }
 }
--- TDDLesson.BankAccount/BankAccount.cs
+++ TDDLesson.BankAccount/BankAccount.cs
@@ -2,12 +2,24 @@
 
 public sealed class BankAccount
 {
-    public int Balance { get; private set; }
+    public int Balance { get; set; }
 
     public void Add(int money)
     {
-        if (money < 0) throw new InvalidOperationException();
+        if (money < 0)
+            throw new ArgumentException("The amount must be greater than zero");
         
-        Balance = money;
+        Balance += money;
+    }
+
+    public void Withdrawals(int money)
+    {
+        if (money < 0)
+            throw new ArgumentException("The amount must be greater than zero");
+
+        if (Balance < money)
+            throw new InvalidOperationException("You are trying to take off more than you have.");
+
+        Balance -= money;
     }
 }
