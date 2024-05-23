5a08298d26c3d03bd30b68ddd0440c48085a1c29
Ilya Kudryashov <kudryashov@mindbox.cloud>
2024-05-23 11:42:26 +0300

    done task 1

--- TDDLesson.BankAccount.Tests/BankAccountTests.cs
+++ TDDLesson.BankAccount.Tests/BankAccountTests.cs
@@ -1,8 +1,110 @@
-﻿namespace TDDLesson.BankAccount.Tests;
+﻿using Moq;
+
+namespace TDDLesson.BankAccount.Tests;
 
 using FluentAssertions;
 
 [TestClass]
 public sealed class BankAccountTests
 {
+    [TestMethod]
+    public void ShouldAddToAccount_WhenValidAmountProvided()
+    {
+        // Arrange
+        var initialAmount = 100;
+        var amountToAdd = 200;
+        var account = new BankAccount(initialAmount);
+        
+        // Act
+        account.Add(amountToAdd);
+        
+        // Assert
+        account.Balance.Should().Be(300);
+    }
+    
+    [TestMethod]
+    public void ShouldThrowException_WhenInvalidAddAmountProvided()
+    {
+        // Arrange
+        var initialAmount = 100;
+        var amountToAdd = -200;
+        var account = new BankAccount(initialAmount);
+        
+        // Act
+        var act = () => account.Add(amountToAdd);
+        
+        // Assert
+        act.Should().Throw<InvalidOperationException>().WithMessage("Incorrect value");
+    }
+    
+    [TestMethod]
+    public void ShouldWithdrawFromAccount_WhenValidAmountProvided()
+    {
+        // Arrange
+        var initialAmount = 200;
+        var amountToWithdraw = 100;
+        var account = new BankAccount(initialAmount);
+        
+        // Act
+        account.Withdraw(amountToWithdraw);
+        
+        // Assert
+        account.Balance.Should().Be(100);
+    }
+    
+    [TestMethod]
+    public void ShouldThrowException_WhenInvalidWithdrawAmountProvided()
+    {
+        // Arrange
+        var initialAmount = 100;
+        var amountToWithdraw = -200;
+        var account = new BankAccount(initialAmount);
+        
+        // Act
+        var act = () => account.Withdraw(amountToWithdraw);
+        
+        // Assert
+        act.Should().Throw<InvalidOperationException>().WithMessage("Incorrect value");
+    }
+    
+    [TestMethod]
+    public void ShouldThrowException_WhenBalanceGreaterThanAmount()
+    {
+        // Arrange
+        var initialAmount = 100;
+        var amountToWithdraw = 101;
+        var account = new BankAccount(initialAmount);
+        
+        // Act
+        var act = () => account.Withdraw(amountToWithdraw);
+        
+        // Assert
+        act.Should().Throw<InvalidOperationException>().WithMessage("Not enough balance");
+    }
+    
+    [TestMethod]
+    public void ShouldThrowException_WhenAccountCreatedWithNegativeBalance()
+    {
+        // Arrange
+        var amount = -1;
+        
+        // Act
+        var act = () => new BankAccount(amount);;
+        
+        // Assert
+        act.Should().Throw<InvalidOperationException>().WithMessage("Cannot create account with negative balance");
+    }
+
+    [TestMethod]
+    public void ShouldCreateBankAccount_WhenAccountCreatedWithZeroBalance()
+    {
+        // Arrange
+        var amount = 0;
+        
+        // Act
+        var account = new BankAccount(amount);;
+        
+        // Assert
+        account.Balance.Should().Be(amount);
+    }
 }
--- TDDLesson.BankAccount/BankAccount.cs
+++ TDDLesson.BankAccount/BankAccount.cs
@@ -4,10 +4,25 @@ public sealed class BankAccount
 {
     public int Balance { get; private set; }
 
+    public BankAccount(int initialAmount)
+    {
+        if (initialAmount < 0) throw new InvalidOperationException("Cannot create account with negative balance");
+        Balance = initialAmount;
+    }
+
     public void Add(int money)
     {
-        if (money < 0) throw new InvalidOperationException();
+        if (money < 0) throw new InvalidOperationException("Incorrect value");
         
-        Balance = money;
+        Balance += money;
+    }
+    
+    public void Withdraw(int money)
+    {
+        if (money < 0) throw new InvalidOperationException("Incorrect value");
+
+        if (Balance < money) throw new InvalidOperationException("Not enough balance");
+        
+        Balance -= money;
     }
 }
