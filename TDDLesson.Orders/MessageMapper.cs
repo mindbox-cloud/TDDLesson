c19a9cb5027e680f6185bd777210257949c3d7c1
Daniil Belov <belov@mindbox.cloud>
2024-05-28 21:33:20 +0400

    Add message mapping

---
+++ TDDLesson.Orders/MessageMapper.cs
@@ -0,0 +1,15 @@
+namespace TDDLesson;
+
+public static class MessageMapper
+{
+    public static (string subject, string message) MapMessage(ProposalStatus proposalStatus)
+    {
+        return proposalStatus switch
+        {
+            ProposalStatus.Declined => (Messages.DeclinedSubject, Messages.DeclinedBody),
+            ProposalStatus.Processed => (Messages.ProcessedSubject, Messages.ProcessedBody),
+            ProposalStatus.ProcessedAndInvited => (Messages.InviteSubject, Messages.InviteBody),
+            _ => throw new ArgumentOutOfRangeException(nameof(proposalStatus))
+        };
+    }
+}
--- TDDLesson.Orders/NotificationRenderer.cs
+++
@@ -1,7 +0,0 @@
-namespace TDDLesson;
-
-public class NotificationRenderer
-{
-    public static (string subject, string body) Render(ProposalDto dto, ProposalStatus status)
-        => throw new NotImplementedException();
-}
