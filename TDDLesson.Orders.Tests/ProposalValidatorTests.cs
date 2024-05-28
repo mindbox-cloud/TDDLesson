712d84ce140c871a30dc2d15084a79248b4d4554
Ilya Krupcov <secretman44444@gmail.com>
2024-05-28 18:27:37 +0300

    Add validator

---
+++ TDDLesson.Orders.Tests/ProposalValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using TDDLesson;
+
+namespace TestProject1;
+
+[TestClass]
+public class ProposalValidatorTests
+{
+    private readonly ProposalValidator _proposalValidator = new();
+    
+    [TestMethod]
+    public void IsProposalValid_IsValid_True()
+    {
+        var request = new ProposalValidationRequest(101, 31);
+
+        var result = _proposalValidator.IsProposalValid(request);
+
+        result.Should().BeTrue();
+    }
+    
+    [TestMethod]
+    [DataRow(100)]
+    [DataRow(50)]
+    public void IsProposalValid_EmployeeAmountIsInvalid_False(int employeeAmount)
+    {
+        var request = new ProposalValidationRequest(employeeAmount, 31);
+
+        var result = _proposalValidator.IsProposalValid(request);
+
+        result.Should().BeFalse();
+    }
+    
+    [TestMethod]
+    [DataRow(30)]
+    [DataRow(15)]
+    public void IsProposalValid_ItRevenuePercentIsInvalid_False(int itRevenuePercent)
+    {
+        var request = new ProposalValidationRequest(101, itRevenuePercent);
+
+        var result = _proposalValidator.IsProposalValid(request);
+
+        result.Should().BeFalse();
+    }
+}
---
+++ TDDLesson.Orders/ProposalValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace TDDLesson;
+
+public class ProposalValidator
+{
+    public bool IsProposalValid(ProposalValidationRequest proposalValidationRequest)
+    {
+        if (proposalValidationRequest.EmployeesAmount <= 100)
+            return false;
+        
+        if (proposalValidationRequest.ItRevenuePercent <= 30)
+            return false;
+        
+        return true;
+    }
+}
