2b725c4da7eb2a8f9c2cae1c5f9c9c040ff34639
Akim Adzhi-Asan <adzhiasan.a@mindbox.cloud>
2024-05-28 17:22:06 +0300

    validation realization added

--- TDDLesson.Orders/ProposalDto.cs
+++ TDDLesson.Orders/ProposalDto.cs
@@ -3,11 +3,11 @@ namespace TDDLesson;
 public sealed record ProposalDto
 {
     public required int CompanyNumber { get; init; }
-    
+
     public required string CompanyName { get; init; }
-    
+
     public required string CompanyEmail { get; init; }
-    
+
     public required int EmployeesAmount { get; init; }
 }
 
@@ -15,17 +15,21 @@ public static class ProposalDtoExtensions
 {
     private const int MinEmployeesAmount = 100;
     private const float MinITRevenuePercent = 30;
+
     public static bool IsAppropriate(
         this ProposalDto proposalDto,
         float revenuePercent)
     {
-        if (!isValid(proposalDto))
+        if (!IsValid(proposalDto))
             return false;
         return proposalDto.EmployeesAmount > MinEmployeesAmount && revenuePercent > MinITRevenuePercent;
     }
-    private static bool isValid(ProposalDto proposalDto)
-    {
-        throw new NotImplementedException();
-    }
 
+    private static bool IsValid(ProposalDto proposalDto)
+    {
+        if (proposalDto.EmployeesAmount < 0 || proposalDto.CompanyNumber < 0)
+            throw new ArgumentException();
+
+        return true;
+    }
 }
