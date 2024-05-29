6e8d502a537a73f40dd959178f8ca0c55e23bd0d
Akim Adzhi-Asan <adzhiasan.a@mindbox.cloud>
2024-05-29 15:52:24 +0300

    add ShouldSentNotificationToForum logic

--- TDDLesson.Orders/NotificationsHelper.cs
+++ TDDLesson.Orders/NotificationsHelper.cs
@@ -2,13 +2,15 @@ namespace TDDLesson;
 
 public static class NotificationsHelper
 {
+   private const int MinimumEmployeesAmountForInvitation = 500;
+   
    public static (string, string, string) BuildInvitationalMessage(Proposal proposal, DateOnly processingDate)
    {
       return ("test@mindbox.cloud", "Invitation to forum", "<p>Hello!</p>");
    }
-   
+
    public static bool ShouldSentNotificationToForum(Proposal proposal, DateOnly processingDate)
    {
-      return false;
+      return proposal.EmployeesAmount > MinimumEmployeesAmountForInvitation;
    }
 }
