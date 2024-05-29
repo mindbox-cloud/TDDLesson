558803404f38f8a66bd496117156c2f6d32d6045
Yusupova Ramilya <yusupova@mindbox.cloud>
2024-05-29 20:25:13 +0300

    Smol adjustment

--- TDDLesson.Orders/AccreditationProposalProcessor.cs
+++ TDDLesson.Orders/AccreditationProposalProcessor.cs
@@ -1,38 +1,29 @@
 ﻿namespace TDDLesson;
 
-public class AccreditationProposalProcessor
+public class AccreditationProposalProcessor(
+    IRevenueService revenueService,
+    IRepository proposalRepository,
+    IEmailClient emailClient,
+    IDateTimeProvider dateTimeProvider)
 {
-    private readonly IRevenueService _revenueService;
-    private readonly IRepository _proposalRepository;
-    private readonly IEmailClient _emailClient;
-    private readonly IDateTimeProvider _dateTimeProvider;
-
-    public AccreditationProposalProcessor(
-        IRevenueService revenueService,
-        IRepository proposalRepository,
-        IEmailClient emailClient,
-        IDateTimeProvider dateTimeProvider)
-    {
-        _revenueService = revenueService;
-        _proposalRepository = proposalRepository;
-        _emailClient = emailClient;
-        _dateTimeProvider = dateTimeProvider;
-    }
-    
     public async Task HandleProposal(ProposalDto dto)
     {
         var proposal = new Proposal(dto);
-        await _proposalRepository.SaveAsync(proposal);
-        var revenue = _revenueService.GetRevenuePercent(proposal.CompanyNumber);
+        await proposalRepository.SaveAsync(proposal);
+        
+        var revenue = revenueService.GetRevenuePercent(proposal.CompanyNumber);
+        
         if (proposal.IsAppropriate(revenue))
         {
             var approvalMessage = MessageBuilder.BuildApprovalMessage(proposal);
-            _emailClient.SendEmail(approvalMessage.Email, approvalMessage.Subject, approvalMessage.Body);
+            await emailClient.SendEmail(approvalMessage.Email, approvalMessage.Subject, approvalMessage.Body);
         }
-        if (ForumInvitationManager.ShouldSentNotificationToForum(proposal, DateOnly.FromDateTime(_dateTimeProvider.GetDateTimeUtcNow())))
+
+        if (ForumInvitationManager.ShouldSentNotificationToForum(proposal,
+                DateOnly.FromDateTime(dateTimeProvider.GetDateTimeUtcNow())))
         {
             var approvalMessage = MessageBuilder.BuildInvitationalMessage(proposal);
-            _emailClient.SendEmail(approvalMessage.Email, approvalMessage.Subject, approvalMessage.Body);
+            await emailClient.SendEmail(approvalMessage.Email, approvalMessage.Subject, approvalMessage.Body);
         }
     }
 }
