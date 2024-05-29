cc70d4745646c84c59d52e913432624a64866d29
Akim Adzhi-Asan <adzhiasan.a@mindbox.cloud>
2024-05-29 16:07:23 +0300

    ShouldSentNotificationToForum realization

--- TDDLesson.Orders/NotificationsHelper.cs
+++ TDDLesson.Orders/NotificationsHelper.cs
@@ -3,6 +3,8 @@ namespace TDDLesson;
 public static class NotificationsHelper
 {
    private const int MinimumEmployeesAmountForInvitation = 500;
+   private static DateOnly RightBound = new DateOnly(2024, 09, 01);
+   private static DateOnly LeftBound = new DateOnly(2024, 06, 01);
    
    public static (string, string, string) BuildInvitationalMessage(Proposal proposal, DateOnly processingDate)
    {
@@ -11,6 +13,8 @@ public static class NotificationsHelper
 
    public static bool ShouldSentNotificationToForum(Proposal proposal, DateOnly processingDate)
    {
-      return proposal.EmployeesAmount > MinimumEmployeesAmountForInvitation;
+      return proposal.EmployeesAmount > MinimumEmployeesAmountForInvitation
+         && processingDate <= RightBound
+         && processingDate >= LeftBound;
    }
 }
